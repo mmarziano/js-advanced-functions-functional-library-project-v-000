a51e2c6007e80a6e54278b8af6c271b4fc7372a9
Monica Marziano <mmarziano1@gmail.com>
2019-10-25 12:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const fi = (function() {
 
     values: function(obj) {
       let values = [];
-      Object.values(obj).map((obj) => console.log(obj.value));
+      Object.values(obj).map((obj) => console.log(obj));
     
       return values;
     },
