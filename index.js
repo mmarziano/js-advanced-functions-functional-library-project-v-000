e7661bc1ddee83bceece68fd7191a7c26a607839
Monica Marziano <mmarziano1@gmail.com>
2019-10-25 11:51:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let newCollection = (collection instanceof Array) ? collection : Object.values(collection)
+      newCollection.map(callback)
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newCollection = (collection instanceof Array) ? collection : Object.values(collection)
+      newCollection.map(callback)
+      return collection
     },
 
     reduce: function() {
